2433020f1a3c01d11291387dbd504d78b07a603a
Jakub <wojcieku@gmail.com>
2024-11-10 14:21:56 +0100

    student name input XSS fix

--- Java/spring-thymeleaf-crud-example/src/main/java/com/example/thymeleaf/dto/CreateStudentDTO.java
+++ Java/spring-thymeleaf-crud-example/src/main/java/com/example/thymeleaf/dto/CreateStudentDTO.java
@@ -6,6 +6,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
@@ -13,6 +14,7 @@ import java.time.LocalDate;
 @Setter
 public class CreateStudentDTO {
 
+    @Pattern(regexp = "^[A-Z][a-z]{0,19}$")
     @NotEmpty(message = "{NotEmpty.name}")
     private String name;
 
